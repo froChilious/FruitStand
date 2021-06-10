5ff04e363b9790769212fcc717d9eeff2487dee2
froChilious <frochilious@gmail.com>
2021-06-10 10:41:21 -0500

    Updated changeImage.py to properly build paths.

--- changeImage.py
+++ changeImage.py
@@ -4,18 +4,22 @@ from PIL import Image
 import glob, os
 
 im_dir = '/home/student/supplier-data/images'
+tiff_dir = r'C:\Users\jhoov\Projects\PIL'
 
 def change_images(dir, from_ext, to_ext):
-
-    for infile in glob.glob(os.path.join(dir,'/*.{}'.from_ext):
+    full_image_path = os.path.join(dir,'*.{}'.format(from_ext))
+    print(full_image_path)
+    for infile in glob.glob(full_image_path):
         try:
-            with Image.open(infile) as im:
+            with Image.open(infile).convert('RGB') as im:
                 file,ext = os.path.splitext(infile)
                 filen = file.split('/')[-1]
-                new_file = '{}.jpeg'.format(file)
+                old_file = '{}.{}'.format(file,from_ext)
+                new_file = '{}.{}'.format(file,to_ext)
                 #print(new_file)
                 im.rotate(180).resize((640,480)).save(new_file,format=to_ext)
-                print ('Image {}.tiff has been processed to {}.jpeg'.format(file,new_file))
+                print ('Image {} has been processed to {}'.format(old_file,new_file))
         except Exception as e:
             print(f'Unable to process the picture {file+ext} with Error: {e}.')
 
+change_images(tiff_dir,'jpg','tiff')
