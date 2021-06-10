bd4541ab61a1ccd2b1d91a4b6b01947872a60312
froChilious <frochilious@gmail.com>
2021-06-10 10:47:11 -0500

    Added resize size to the inputs, set rotate to 0.

--- changeImage.py
+++ changeImage.py
@@ -3,10 +3,14 @@
 from PIL import Image
 import glob, os
 
-im_dir = '/home/student/supplier-data/images'
-tiff_dir = r'C:\Users\jhoov\Projects\PIL'
+#im_dir = '/home/student/supplier-data/images'
 
-def change_images(dir, from_ext, to_ext):
+def change_images(dir, from_ext, to_ext, resize=(600,400)):
+    '''
+    Search the given path for all image files with the from_ext and
+    convert them to the to_ext format while also re-sizing the image 
+    to 640x480. Write them back out to the same directory.
+    '''
     full_image_path = os.path.join(dir,'*.{}'.format(from_ext))
     print(full_image_path)
     for infile in glob.glob(full_image_path):
@@ -17,9 +21,9 @@ def change_images(dir, from_ext, to_ext):
                 old_file = '{}.{}'.format(file,from_ext)
                 new_file = '{}.{}'.format(file,to_ext)
                 #print(new_file)
-                im.rotate(180).resize((640,480)).save(new_file,format=to_ext)
+                im.rotate(0).resize(resize).save(new_file,format=to_ext)
                 print ('Image {} has been processed to {}'.format(old_file,new_file))
         except Exception as e:
             print(f'Unable to process the picture {file+ext} with Error: {e}.')
 
-change_images(tiff_dir,'jpg','tiff')
+#change_images(tiff_dir,'tiff','jpeg')
