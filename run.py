17a3eae1eb98cbea4fb1addd4c90c2a5d53b8df9
froChilious <frochilious@gmail.com>
2021-06-10 18:26:50 -0500

    small changes to health_check.py

--- run.py
+++ run.py
@@ -1,21 +1,27 @@
-
+#!/usr/bin/env python3
 import os, requests
 
-payload = {'key1': 'value1', 'key2': 'value2'}
-r = requests.get('https://httpbin.org/get', params=payload)
-print(r.raise_for_status())
-files = r'C:\Users\jhoov\Projects\FruitStand'
-url = 'http://34.16.135.157/fruits' # not an actual url
+description_dir = '/home/student-01-d1a9ac6f5b91/supplier-data/descriptions/'
+image_dir = '/home/student-01-d1a9ac6f5b91/supplier-data/descriptions/'
+
+url = 'http://localhost/fruits/'
 imap = ['name','weight','description','image_name']
 
-def get_fruit_info(file_dir, imap):
+def get_fruit_info(file_dir, image_dir, imap):
     fruit_info = []
     for file in os.listdir(file_dir):
         if file.split('.')[-1] == 'txt':
             fruit = {}
             with open(os.path.join(file_dir,file),'r') as f:
                 for i,line in enumerate(f.readlines()):
-                    fruit[imap[i]] = line.strip()
+                    if i == 1:
+                        info = line.strip().split()[0]
+                    else:
+                        info = line.strip()
+                    if i < 3:
+                        fruit[imap[i]] = info
+                image_file = os.path.join(image_dir,'{}{}'.format(file.split('.')[0],'.jpeg'))
+                fruit[imap[3]] = image_file
             fruit_info.append(fruit)
     return fruit_info
 
@@ -27,5 +33,6 @@ def upload_descriptions(url, fruit_data):
             print(err)
     
 
-fruit_data = get_fruit_info(files, imap)
+fruit_data = get_fruit_info(description_dir, image_dir, imap)
+print(fruit_data[0])
 upload_descriptions(url,fruit_data)
