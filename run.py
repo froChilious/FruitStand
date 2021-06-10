6defe4191e1fca8076e0fe8476b2c36e5a414841
froChilious <frochilious@gmail.com>
2021-06-10 15:15:46 -0500

    Split run into a dictionary build to hold the fruit info and a separate upload function to run the post method

--- run.py
+++ run.py
@@ -1,20 +1,31 @@
 
 import os, requests
 
+payload = {'key1': 'value1', 'key2': 'value2'}
+r = requests.get('https://httpbin.org/get', params=payload)
+print(r.raise_for_status())
 files = r'C:\Users\jhoov\Projects\FruitStand'
 url = 'http://34.16.135.157/fruits' # not an actual url
 imap = ['name','weight','description','image_name']
 
-def upload_descriptions(file_dir, url, fieldmap):
+def get_fruit_info(file_dir, imap):
+    fruit_info = []
     for file in os.listdir(file_dir):
         if file.split('.')[-1] == 'txt':
-            print(file)
-            fruit_info = {}
+            fruit = {}
             with open(os.path.join(file_dir,file),'r') as f:
                 for i,line in enumerate(f.readlines()):
-                    fruit_info[imap[i]] = line.strip()
-            response = requests.post(url,data=fruit_info)
-            #print(response.raise_for_status())
-            #print(f'{response.request.url}?{response.request.body}')
+                    fruit[imap[i]] = line.strip()
+            fruit_info.append(fruit)
+    return fruit_info
 
-#upload_descriptions(files, url, imap)
+def upload_descriptions(url, fruit_data):
+    for fruit in fruit_data:
+        try:
+            response = requests.post(url,data=fruit)
+        except Exception as err:
+            print(err)
+    
+
+fruit_data = get_fruit_info(files, imap)
+upload_descriptions(url,fruit_data)
