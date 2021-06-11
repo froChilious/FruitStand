403c2baa8ca1b8b95c3586ae3ef19e29d7ee89cb
froChilious <frochilious@gmail.com>
2021-06-11 11:47:26 -0500

    Added report_email.py, renamed from emails.py

--- report_email.py
+++ report_email.py
@@ -19,18 +19,26 @@ def get_fruit_report_parts(fruit_data):
     report_title = "Processed Update on {}".format(today)
     fruit_body = ''
     for fruit in fruit_data:
-        fruit_body = '{}{}<br></br>{}<br></br><br></br>'.format(fruit_body,fruit['name'],fruit['weight'])
+        fruit_body = '{}<br></br>name: {}<br></br>weight: {}'.format(fruit_body,fruit['name'],fruit['weight'])
     return report_title, fruit_body
 
 def main():
-    descriptions_dir = '/home/student/supplier-data/descriptions/'
+    user = 'student-03-32beb94feb5b'
+    descriptions_dir = '/home/{}/supplier-data/descriptions/'.format(user)
     url = 'http://localhost/fruits/' # not an actual url
     imap = ['name','weight','description','image_name']
-
+    # Get the data from the files and make a dictionary
     fruit_data = get_fruit_info(descriptions_dir, imap)
     title, paragraph = get_fruit_report_parts(fruit_data)
-
-    reports.generate_report('processed.pdf', title, paragraph)
+    # Generate the PDF report
+    report_pdf = 'processed.pdf'
+    reports.generate_report(report_pdf, title, paragraph)
+    # Generate and send the email
+    sender = 'automation@example.com'
+    recipient = '{}@example.com'.format(user)
+    subject = 'Upload Completed - Online Fruit Store'
+    body = 'All fruits are uploaded to our website successfully. A detailed list is attached to this email.'
+    message = emails.generate_email(sender,recipient,subject,body,report_pdf)
 
 if __name__ == '__main__':
     main()
